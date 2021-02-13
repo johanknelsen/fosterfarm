577e68328732044ca1d6511c473eda80102640a4
johanknelsen <46053897+johanknelsen@users.noreply.github.com>
2021-02-13 14:26:19 -0500

    Update wsgi.py

--- fosterfarm/wsgi.py
+++ fosterfarm/wsgi.py
@@ -13,8 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fosterfarm.settings')
 
-from whitenoise.django import DjangoWhiteNoise
 
-application = DjangoWhiteNoise(get_wsgi_application())
 
 application = get_wsgi_application()
